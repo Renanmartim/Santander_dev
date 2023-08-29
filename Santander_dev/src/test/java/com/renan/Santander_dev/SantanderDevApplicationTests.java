233d7358a3f1a43c02aad6ecd4abef072ddb6971
Renan <renan_camaro@hotmail.com>
2023-08-29 03:52:04 -0300

    Add Spring

---
+++ Santander_dev/src/test/java/com/renan/Santander_dev/SantanderDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renan.Santander_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
